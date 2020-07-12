78b82f5793186ba39f2d7de459f86c9bcf02943f
ashok parihar <aparihar@email.arizona.edu>
2020-07-11 21:00:49 -0600

    Adding PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -8,14 +8,6 @@
 #Assign a variable for the file to load and the path.
 file_to_load = 'Resources/election_results.csv'
 
-#Open the election results and read the file.
-election_data = open(file_to_load, 'r')
-
-#To do: perform analysis.
-
-#Close the file.
-election_data.close()
-
 #Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -25,60 +17,19 @@ with open(file_to_load) as election_data:
 #Add our dependencies
 import csv
 import os
-#Assign a variable for the file to load the path.
+# Assign a variable for the file to load the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-
-#Using the with statement to open the file as a text file.
-with open(file_to_load) as election_data:
-
-    #Print file to object.
-    print(election_data)
-
 # Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-
-    # Write three counties to the file.
-     txt_file.write("Counties in the Election\n-------------------------")
-    
-    # Write three counties to the file.
-     txt_file.write("\nArapahoe\nDenver\nJefferson")
-
-# Add our dependencies.
-import csv
-import os
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: read and analyze the data here.
-    # Read the file object with the reader function.
+# To do: read and analyze the data here.
+# Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+# Print the header row.
     headers = next(file_reader)
     print(headers)
     
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
